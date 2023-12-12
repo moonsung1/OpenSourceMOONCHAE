585753020112b72ee33ed73f3bf497dec4b01e3f
jiwon0751 <jiwon0751@naver.com>
2023-12-13 02:42:31 +0900

    Creon에서 정보 가져오기

--- AutoTrade.py
+++ AutoTrade.py
@@ -1,12 +1,26 @@
 import os, sys, ctypes
 import win32com.client
+import pandas as pd
 from datetime import datetime
+from slacker import Slacker
+from urllib.request import urlopen
+import requests
+
+def post_message(token, channel, answer):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": answer}
+    )
+
+def dbgout(message):
+    """인자로 받은 문자열을 파이썬 셸과 슬랙으로 동시에 출력한다."""
+    print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message)
+    strbuf = datetime.now().strftime('[%m/%d %H:%M:%S] ') + message
+    post_message('xoxb-6296035588643-6281662619047-sHnJYQHFM8GQvnbnClQBltMq', '#etf-algo-trading', strbuf)
 
 def printlog(message, *args):
-    """인자로 받은 문자열을 파이썬 셸에 출력한다."""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args)
 
-# 크레온 플러스 공통 OBJECT
 cpCodeMgr = win32com.client.Dispatch('CpUtil.CpStockCode')
 cpStatus = win32com.client.Dispatch('CpUtil.CpCybos')
 cpTradeUtil = win32com.client.Dispatch('CpTrade.CpTdUtil')
@@ -17,29 +31,120 @@ cpCash = win32com.client.Dispatch('CpTrade.CpTdNew5331A')
 cpOrder = win32com.client.Dispatch('CpTrade.CpTd0311')  
 
 def check_creon_system():
-    """크레온 플러스 시스템 연결 상태를 점검한다."""
-    # 관리자 권한으로 프로세스 실행 여부
     if not ctypes.windll.shell32.IsUserAnAdmin():
         printlog('check_creon_system() : admin user -> FAILED')
         return False
- 
-    # 연결 여부 체크
+
     if (cpStatus.IsConnect == 0):
         printlog('check_creon_system() : connect to server -> FAILED')
         return False
  
-    # 주문 관련 초기화 - 계좌 관련 코드가 있을 때만 사용
     if (cpTradeUtil.TradeInit(0) != 0):
         printlog('check_creon_system() : init trade -> FAILED')
         return False
     return True
 
 def get_current_price(code):
-    """인자로 받은 종목의 현재가, 매도호가, 매수호가를 반환한다."""
-    cpStock.SetInputValue(0, code)  # 종목코드에 대한 가격 정보
+    cpStock.SetInputValue(0, code) 
     cpStock.BlockRequest()
     item = {}
-    item['cur_price'] = cpStock.GetHeaderValue(11)   # 현재가
-    item['ask'] =  cpStock.GetHeaderValue(16)        # 매도호가
-    item['bid'] =  cpStock.GetHeaderValue(17)        # 매수호가    
+    item['cur_price'] = cpStock.GetHeaderValue(11)   
+    item['ask'] =  cpStock.GetHeaderValue(16)       
+    item['bid'] =  cpStock.GetHeaderValue(17)         
     return item['cur_price'], item['ask'], item['bid']
+
+def get_ohlc(code, qty):
+    cpOhlc.SetInputValue(0, code)           # 종목코드
+    cpOhlc.SetInputValue(1, ord('2'))        # 1:기간, 2:개수
+    cpOhlc.SetInputValue(4, qty)             # 요청개수
+    cpOhlc.SetInputValue(5, [0, 2, 3, 4, 5]) # 0:날짜, 2~5:OHLC
+    cpOhlc.SetInputValue(6, ord('D'))        # D:일단위
+    cpOhlc.SetInputValue(9, ord('1'))        # 0:무수정주가, 1:수정주가
+    cpOhlc.BlockRequest()
+    count = cpOhlc.GetHeaderValue(3)   # 3:수신개수
+    columns = ['open', 'high', 'low', 'close']
+    index = []
+    rows = []
+    for i in range(count): 
+        index.append(cpOhlc.GetDataValue(0, i)) 
+        rows.append([cpOhlc.GetDataValue(1, i), cpOhlc.GetDataValue(2, i),
+            cpOhlc.GetDataValue(3, i), cpOhlc.GetDataValue(4, i)]) 
+    df = pd.DataFrame(rows, columns=columns, index=index) 
+    return df
+
+def get_stock_balance(code):
+    cpTradeUtil.TradeInit()
+    acc = cpTradeUtil.AccountNumber[0]      # 계좌번호
+    accFlag = cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
+    cpBalance.SetInputValue(0, acc)         # 계좌번호
+    cpBalance.SetInputValue(1, accFlag[0])  # 상품구분 - 주식 상품 중 첫번째
+    cpBalance.SetInputValue(2, 50)          # 요청 건수(최대 50)
+    cpBalance.BlockRequest()     
+    if code == 'ALL':
+        dbgout('계좌명: ' + str(cpBalance.GetHeaderValue(0)))
+        dbgout('결제잔고수량 : ' + str(cpBalance.GetHeaderValue(1)))
+        dbgout('평가금액: ' + str(cpBalance.GetHeaderValue(3)))
+        dbgout('평가손익: ' + str(cpBalance.GetHeaderValue(4)))
+        dbgout('종목수: ' + str(cpBalance.GetHeaderValue(7)))
+    stocks = []
+    for i in range(cpBalance.GetHeaderValue(7)):
+        stock_code = cpBalance.GetDataValue(12, i)  # 종목코드
+        stock_name = cpBalance.GetDataValue(0, i)   # 종목명
+        stock_qty = cpBalance.GetDataValue(15, i)   # 수량
+        if code == 'ALL':
+            dbgout(str(i+1) + ' ' + stock_code + '(' + stock_name + ')' 
+                + ':' + str(stock_qty))
+            stocks.append({'code': stock_code, 'name': stock_name, 
+                'qty': stock_qty})
+        if stock_code == code:  
+            return stock_name, stock_qty
+    if code == 'ALL':
+        return stocks
+    else:
+        stock_name = cpCodeMgr.CodeToName(code)
+        return stock_name, 0
+    
+def get_current_cash():
+    cpTradeUtil.TradeInit()
+    acc = cpTradeUtil.AccountNumber[0]    # 계좌번호
+    accFlag = cpTradeUtil.GoodsList(acc, 1) # -1:전체, 1:주식, 2:선물/옵션
+    cpCash.SetInputValue(0, acc)              # 계좌번호
+    cpCash.SetInputValue(1, accFlag[0])      # 상품구분 - 주식 상품 중 첫번째
+    cpCash.BlockRequest() 
+    return cpCash.GetHeaderValue(9) # 증거금 100% 주문 가능 금액
+
+def get_target_price(code):
+    try:
+        time_now = datetime.now()
+        str_today = time_now.strftime('%Y%m%d')
+        ohlc = get_ohlc(code, 10)
+        if str_today == str(ohlc.iloc[0].name):
+            today_open = ohlc.iloc[0].open 
+            lastday = ohlc.iloc[1]
+        else:
+            lastday = ohlc.iloc[0]                                      
+            today_open = lastday[3]
+        lastday_high = lastday[1]
+        lastday_low = lastday[2]
+        target_price = today_open + (lastday_high - lastday_low) * 0.5
+        return target_price
+    except Exception as ex:
+        dbgout("`get_target_price() -> exception! " + str(ex) + "`")
+        return None
+    
+def get_movingaverage(code, window):
+    """인자로 받은 종목에 대한 이동평균가격을 반환한다."""
+    try:
+        time_now = datetime.now()
+        str_today = time_now.strftime('%Y%m%d')
+        ohlc = get_ohlc(code, 20)
+        if str_today == str(ohlc.iloc[0].name):
+            lastday = ohlc.iloc[1].name
+        else:
+            lastday = ohlc.iloc[0].name
+        closes = ohlc['close'].sort_index()         
+        ma = closes.rolling(window=window).mean()
+        return ma.loc[lastday]
+    except Exception as ex:
+        dbgout('get_movingavrg(' + str(window) + ') -> exception! ' + str(ex))
+        return None  
